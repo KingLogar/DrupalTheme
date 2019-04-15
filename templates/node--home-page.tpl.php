239144fdc3c817e5a5e435ef4041f16e6da633cb
Logan <lh252@njit.edu>
2019-04-14 22:47:13 -0400

    Task: styling

--- templates/node--home-page.tpl.php
+++ templates/node--home-page.tpl.php
@@ -90,31 +90,3 @@ print $classes;
 
 print $attributes;
 ?>>
-
-    <div class="title">
-        <?php print $title; ?>
-    </div>
-
-    <div class="main_image">
-        <?php print render($content['field_main_image']); ?>
-    </div>
-
-    <div class="date">
-        <?php print render($content['field_date']); ?>
-    </div>
-
-    <div class="location"
-        <?php print render($content['field_location']); ?>
-    </div>
-
-    <div class="body">
-        <?php print render($content['body']); ?>
-    </div>
-
-    <div class="agenda">
-        <?php print render($content['field_agenda']); ?>
-    </div>
-
-    <div class="body">
-    <?php print render($content['body']); ?>
-    </div>
